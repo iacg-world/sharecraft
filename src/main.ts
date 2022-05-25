f8a9a7178087fedb0cc14b6771597d10ce718a7a
lc <1033581609@qq.com>
2022-05-25 09:47:47 +0800

    add: ant组件按需引入

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue'
+import { Button, Layout, Card, Col, Row } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css' // or 'ant-design-vue/dist/antd.less'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Button)
+  .use(Layout)
+  .use(Card)
+  .use(Col)
+  .use(Row)
+  .mount('#app')
