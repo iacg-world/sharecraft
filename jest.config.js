a24f3467b5966f9ff08ad8af5e8114f682365256
chao.luo3 <chao.luo3@zhenai.com>
2022-05-27 10:45:22 +0800

    feat: 配置vue3-jest单元测试环境

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+  },
+  transformIgnorePatterns: ['/!node_modules\\/lodash-es/'],
 }
