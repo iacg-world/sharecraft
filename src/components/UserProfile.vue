5fd4a6de8bf440ea01f4a10f854760ade27c5dd3
supercll <1033581609@qq.com>
2023-05-23 18:58:49 +0800

    feat: error btn mock error

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,6 +3,19 @@
     <a-button type="primary" class="user-profile-component"> 登录 </a-button>
   </router-link>
   <div v-else class="home_menu">
+    <a-dropdown class="user-profile-error_button">
+      <template #overlay>
+        <a-menu>
+          <a-menu-item key="0" @click="asyncError">同步错误</a-menu-item>
+          <a-menu-item key="1" @click="promiseError">Promise错误</a-menu-item>
+          <a-menu-item key="2" @click="apiError">接口错误</a-menu-item>
+        </a-menu>
+      </template>
+      <a-button>
+        错误触发器
+        <DownOutlined />
+      </a-button>
+    </a-dropdown>
     <a-button v-if="$route.path === '/'"
       ><router-link to="/works">我的作品</router-link></a-button
     >
@@ -16,11 +29,10 @@
           <a-menu-item key="1"
             ><router-link to="/works">我的作品</router-link></a-menu-item
           >
-          <a-menu-item key="2" @click="logout">登出</a-menu-item>
+          <a-menu-item key="2" @click="logout">退出登录</a-menu-item>
         </a-menu>
       </template>
     </a-dropdown-button>
-
     <a
       style="color: white; font-size: 20px; margin-left: 10px"
       href="https://github.com/iacg-world/sharecraft"
@@ -30,51 +42,55 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
+<script lang="ts" setup>
+import { defineComponent, PropType, defineProps } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { message } from 'ant-design-vue'
 import { UserProps } from '../store/user'
-import { GithubFilled } from '@ant-design/icons-vue'
+import { DownOutlined, GithubFilled } from '@ant-design/icons-vue'
+import axios from 'axios'
 
-export default defineComponent({
-  name: 'user-profile',
-  props: {
-    user: {
-      type: Object as PropType<UserProps>,
-      required: true,
-    },
+defineProps({
+  user: {
+    type: Object as PropType<UserProps>,
+    required: true,
   },
-  setup() {
-    const store = useStore()
-    const router = useRouter()
-    const logout = () => {
-      store.commit('logout')
-      message.success('退出登录成功', 2)
-      setTimeout(() => {
-        router.push('/')
-      }, 2000)
-    }
-    const createDesign = async () => {
-      const payload = {
-        title: '未命名作品',
-        desc: '未命名作品',
-        coverImg: '',
-      }
-      const { data } = await store.dispatch('createWork', {
-        data: payload,
-      })
-      message.success('创建作品成功', 2)
-      router.push(`/editor/${data.id}`)
-    }
-    return {
-      logout,
-      createDesign,
-    }
-  },
-  components: { GithubFilled },
 })
+
+defineComponent({ GithubFilled, DownOutlined })
+
+const store = useStore()
+const router = useRouter()
+const logout = () => {
+  store.commit('logout')
+  message.success('退出登录成功', 2)
+  setTimeout(() => {
+    router.push('/')
+  }, 2000)
+}
+const createDesign = async () => {
+  const payload = {
+    title: '未命名作品',
+    desc: '未命名作品',
+    coverImg: '',
+  }
+  const { data } = await store.dispatch('createWork', {
+    data: payload,
+  })
+  message.success('创建作品成功', 2)
+  router.push(`/editor/${data.id}`)
+}
+
+const asyncError = () => {
+  throw 'async error'
+}
+const promiseError = () => {
+  Promise.reject('promise error')
+}
+const apiError = () => {
+  axios.get('/404')
+}
 </script>
 <style lang="scss">
 .home_menu {
@@ -85,6 +101,10 @@ export default defineComponent({
 
   .user-profile-container {
     margin-left: 50px !important;
+  }
+
+  .user-profile-error_button {
+    margin-right: 10px;
   }
 }
 .user-profile-dropdown {
